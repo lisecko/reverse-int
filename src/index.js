469791bbd044f3acc73bf819a226fcdcb435b0f4
Uladzislau Lisetski <lisetskiywlad@gmail.com>
2020-10-13 09:28:14 +0300

    init: start and completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let str = n.toString()
+  let result = ''
+  for(let i = str.length - 1; i >= 0; i--)
+  {
+    result += str[i]
+  }
+
+  return parseInt(result)
 }
